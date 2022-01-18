c78f20681cf4d1b1427fee16ca6d249fe2e55e4e
danilo costa <danilodscosta1@gmail.com>
2022-01-17 22:21:56 -0300

    api23

--- api.js
+++ api.js
@@ -2,10 +2,10 @@ const { request, response } = require('express')
 const cors = require('cors')
 const express = require('express')
 const res = require('express/lib/response')
-app.use(cors({ origin: '*' }));
 const gerenciadorStarWars = require('./controllers/functions')
 const app = express()
 const swapController = require('./controllers/swapi')
+app.use(cors({ origin: '*' }));
 app.use(express.json())
 
 app.get('/api/people', async (request,response) => {
