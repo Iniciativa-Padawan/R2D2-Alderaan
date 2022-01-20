c5f16bb0a0458022b97910dbd57a2957e131901f
Rafael <rafaelaraujo.adv1@gmail.com>
2022-01-20 13:21:27 -0300

    alteracao

--- db/movies.js
+++ db/movies.js
@@ -1,13 +1,13 @@
 var filmes = [
   
     {nome: "uma nova esperança",
-    sequential: 4, type: "original"},
+    sequential: 4, type: "classic"},
 
     {nome: "o império contra-ataca",
-    sequential: 5, type: "original"},
+    sequential: 5, type: "classic"},
 
     {nome: "o retorno de jedi",
-    sequential: 6, type: "original"},
+    sequential: 6, type: "classic"},
 
     {nome: "a ameaça fantasma",
     sequential: 1, type: "prequel"},
@@ -19,13 +19,13 @@ var filmes = [
     sequential: 3, type: "prequel"},
 
     {nome: "o despertar da força",
-    sequential: 7, type: "atual"},
+    sequential: 7, type: "sequel"},
 
     {nome: "os últimos jedi",
-    sequential: 8, type: "atual"},
+    sequential: 8, type: "sequel"},
 
     {nome: "a ascenção skywalker",
-    sequential: 9, type: "atual"}
+    sequential: 9, type: "sequel"}
     ]
 
     
