5508792a8088d629a59efda0ecddf56a9a154bc6
jminervino <joaovictor_minervino@hotmail.com>
2022-01-20 14:12:13 -0300

    Add sequential property and type

--- ProjectR2D2/db/atributes.js
+++ ProjectR2D2/db/atributes.js
@@ -1,12 +1,18 @@
 const movies = [
     {
+        sequential: 4,
         name: "A New Hope",
+        type: "Classic"
     },  
     {
+        sequential: 5,
         name: "The Empire Strikes Back",
+        type: "Classic"
     },     
     {   
+        sequential: 6,
         name: "Return of Jedi",
+        type: "Classic"
     },
 ]
 
