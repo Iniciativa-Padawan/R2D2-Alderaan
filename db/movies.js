4a93cbf6b4b68c8352198e7bea12e076372dfdf2
Rafael <rafaelaraujo.adv1@gmail.com>
2022-01-20 12:02:35 -0300

    edicao do api

--- db/movies.js
+++ db/movies.js
@@ -1,25 +1,36 @@
 var filmes = [
   
     {nome: "uma nova esperança",
-    ano: 1977,},
+    ano: 1977, type: "original"},
+
     {nome: "o império contra-ataca",
-    ano: 1980,},
+    ano: 1980, type: "original"},
+
     {nome: "o retorno de jedi",
-    ano: 1983,},
+    ano: 1983, type: "original"},
+
     {nome: "a ameaça fantasma",
-    ano: 1999,},
+    ano: 1999, type: "prequel"},
+
     {nome: "ataque dos clones",
-    ano: 2002,},
+    ano: 2002, type: "prequel"},
+
     {nome: "a vingança dos sith",
-    ano: 2005,},
+    ano: 2005, type: "prequel"},
+
     {nome: "o despertar da força",
-    ano: 2015,},
+    ano: 2015, type: "atual"},
+
     {nome: "os últimos jedi",
-    ano: 2017,},
+    ano: 2017, type: "atual"},
+
     {nome: "a ascenção skywalker",
-    ano: 2019,}
+    ano: 2019, type: "atual"}
     ]
 
+    
     function listarfilmes () {
       return filmes
   }
+ 
+  listarfilmes
