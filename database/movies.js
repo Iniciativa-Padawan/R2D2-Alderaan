8d9af24f8bbc6e31dbb6b030f0759a22e38bb9b0
gabrielpser <gabrielpser@gmail.com>
2022-01-23 20:23:37 -0300

    Big changes, CRUD & Swapi Manager ok!

--- database/movies.js
+++ database/movies.js
@@ -1,143 +1,143 @@
 const movieList = [
     {
-        count: 1,
-        data: [
+        "count": 1,
+        "data": [
                         {
-                    name: "A New Hope",
-                    year: 1977,
-                    type: "Classic",
-                    sequential: 1,
-                    index: 0    
+                    "name": "A New Hope",
+                    "year": 1977,
+                    "type": "Classic",
+                    "sequential": 1,
+                    "index": 0    
                         }
                     ]
 },
 
     {
-        count: 2,
-        data: [
+        "count": 2,
+        "data": [
                         {
-                    name: "The Empire Strikes Back",
-                    year: 1980,
-                    type: "Classic",
-                    sequential: 2,
-                    index: 1
+                    "name": "The Empire Strikes Back",
+                    "year": 1980,
+                    "type": "Classic",
+                    "sequential": 2,
+                    "index": 1
                         }
                     ]
 },                 
 
     {
-        count: 3,
-        data: [
+        "count": 3,
+        "data": [
                         {
-                    name: "Return of The Jedi",
-                    year: 1983,
-                    type: "Classic",
-                    sequential: 6,
-                    index: 2
+                    "name": "Return of The Jedi",
+                    "year": 1983,
+                    "type": "Classic",
+                    "sequential": 6,
+                    "index": 2
                         }
                     ]        
 },
 
     {
-        count: 4,
-        data: [
+        "count": 4,
+        "data": [
                         {
-                    name: "The Phantom Menace",
-                    year: 1999,
-                    type: "Prequel",
-                    sequential: 3,
-                    index: 3
+                    "name": "The Phantom Menace",
+                    "year": 1999,
+                    "type": "Prequel",
+                    "sequential": 3,
+                    "index": 3
                         }
                     ]
 },
 
     {
-        count: 5,
-        data: [
+        "count": 5,
+        "data": [
                         {
-                    name: "Atack of The Clones",
-                    year: 2002,
-                    type: "Prequel",
-                    sequential: 4,
-                    index: 4
+                    "name": "Atack of The Clones",
+                    "year": 2002,
+                    "type": "Prequel",
+                    "sequential": 4,
+                    "index": 4
                         }
                     ]
 },
 
     {
-        count: 6,
-        data: [
+        "count": 6,
+        "data": [
                         {
-                    name: "Revenge of The Sith",
-                    year: 2005,
-                    type: "Prequel",
-                    sequential: 5,
-                    index: 5
+                    "name": "Revenge of The Sith",
+                    "year": 2005,
+                    "type": "Prequel",
+                    "sequential": 5,
+                    "index": 5
                         }
                     ]
 },
 
     {
-        count: 7,
-        data: [
+        "count": 7,
+        "data": [
                         {
-                    name: "The Force Awakens",
-                    year: 2015,
-                    type: "Sequel",
-                    sequential: 7,
-                    index: 6
+                    "name": "The Force Awakens",
+                    "year": 2015,
+                    "type": "Sequel",
+                    "sequential": 7,
+                    "index": 6
                         }
                     ]
 },
 
     {
-        count: 8,
-        data: [
+        "count": 8,
+        "data": [
                         {
-                    name: "The Last Jedi",
-                    year: 2017,
-                    type: "Sequel",
-                    sequential: 8,
-                    index: 7
+                    "name": "The Last Jedi",
+                    "year": 2017,
+                    "type": "Sequel",
+                    "sequential": 8,
+                    "index": 7
                         }
                     ]
 },
 
     {
-        count: 9,
-        data: [
+        "count": 9,
+        "data": [
                         {
-                    name: "The Rise of Skywalker",
-                    year: 2019,
-                    type: "Sequel",
-                    sequential: 9,
-                    index: 8
+                    "name": "The Rise of Skywalker",
+                    "year": 2019,
+                    "type": "Sequel",
+                    "sequential": 9,
+                    "index": 8
                         }
                     ]
 },
     
     {
-        count: 10,
-        data: [
+        "count": 10,
+        "data": [
                         {
-                    name: "Rogue One",
-                    year: 2016,
-                    type: "Spin-Off",
-                    sequential: 10,
-                    index: 9
+                    "name": "Rogue One",
+                    "year": 2016,
+                    "type": "Spin-Off",
+                    "sequential": 10,
+                    "index": 9
                         }
                     ]
 },
     
     {
-        count: 11,
-        data: [
+        "count": 11,
+        "data": [
                         {
-                    name: "Han Solo",
-                    year: 2018,
-                    type: "Spin-Off",
-                    sequential: 11,
-                    index: 10
+                    "name": "Han Solo",
+                    "year": 2018,
+                    "type": "Spin-Off",
+                    "sequential": 11,
+                    "index": 10
                         }
                     ]
 },
