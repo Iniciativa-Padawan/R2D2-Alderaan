d7957d0d5ebad71c7f4a941ef178d60ca437ef62
gabrielpser <gabrielpser@gmail.com>
2022-01-23 16:39:42 -0300

    new list

--- database/movies.js
+++ database/movies.js
@@ -1,35 +1,149 @@
 const movieList = [
     {
-        name: "A New Hope",
-        year: 1977,
-        sequential: 4
-    }, 
+        count: 1,
+        data: [
+                        {
+                    name: "A New Hope",
+                    year: 1977,
+                    type: "Classic",
+                    sequential: 1,
+                    index: 0    
+                        }
+                    ]
+},
+
     {
-        name: "The Empire Strikes Back",
-        year: 1980,
-        sequential: 5
-    }, 
+        count: 2,
+        data: [
+                        {
+                    name: "The Empire Strikes Back",
+                    year: 1980,
+                    type: "Classic",
+                    sequential: 2,
+                    index: 1
+                        }
+                    ]
+},                 
+
     {
-        name: "Return of The Jedi",
-        year: 1983,
-        sequential: 6
-    },
+        count: 3,
+        data: [
+                        {
+                    name: "Return of The Jedi",
+                    year: 1983,
+                    type: "Classic",
+                    sequential: 6,
+                    index: 2
+                        }
+                    ]        
+},
+
     {
-        name: "Revenge of The Sith",
-        year: 2005,
-        sequential: 3
-    },
+        count: 4,
+        data: [
+                        {
+                    name: "The Phantom Menace",
+                    year: 1999,
+                    type: "Prequel",
+                    sequential: 3,
+                    index: 3
+                        }
+                    ]
+},
+
     {
-        name: "Atack of The Clones",
-        year: 2002,
-        sequential: 2
-    },
+        count: 5,
+        data: [
+                        {
+                    name: "Atack of The Clones",
+                    year: 2002,
+                    type: "Prequel",
+                    sequential: 4,
+                    index: 4
+                        }
+                    ]
+},
+
     {
-        name: "The Phantom Menace",
-        year: 1999,
-        sequential: 1
-    }
+        count: 6,
+        data: [
+                        {
+                    name: "Revenge of The Sith",
+                    year: 2005,
+                    type: "Prequel",
+                    sequential: 5,
+                    index: 5
+                        }
+                    ]
+},
+
+    {
+        count: 7,
+        data: [
+                        {
+                    name: "The Force Awakens",
+                    year: 2015,
+                    type: "Sequel",
+                    sequential: 7,
+                    index: 6
+                        }
+                    ]
+},
+
+    {
+        count: 8,
+        data: [
+                        {
+                    name: "The Last Jedi",
+                    year: 2017,
+                    type: "Sequel",
+                    sequential: 8,
+                    index: 7
+                        }
+                    ]
+},
+
+    {
+        count: 9,
+        data: [
+                        {
+                    name: "The Rise of Skywalker",
+                    year: 2019,
+                    type: "Sequel",
+                    sequential: 9,
+                    index: 8
+                        }
+                    ]
+},
+    
+    {
+        count: 10,
+        data: [
+                        {
+                    name: "Rogue One",
+                    year: 2016,
+                    type: "Spin-Off",
+                    sequential: 10,
+                    index: 9
+                        }
+                    ]
+},
+    
+    {
+        count: 11,
+        data: [
+                        {
+                    name: "Han Solo",
+                    year: 2018,
+                    type: "Spin-Off",
+                    sequential: 11,
+                    index: 10
+                        }
+                    ]
+},
+
 ]
 
+    
 
 module.exports =  movieList 
