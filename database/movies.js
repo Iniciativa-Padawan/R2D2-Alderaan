bb144afea4fcd85b6663b7e5bb48ecca0ba5fcb2
gabrielpser <gabrielpser@gmail.com>
2022-01-24 12:14:24 -0300

    Count/data fixed

--- database/movies.js
+++ database/movies.js
@@ -1,6 +1,6 @@
-const movieList = [
+const movieList = 
     {
-    "count": 11,
+        "count": 11,
         "data": [
                         {
                     "name": "A New Hope",
@@ -8,123 +8,72 @@ const movieList = [
                     "type": "Classic",
                     "sequential": 1,
                     "index": 0    
-                        }
-                    ]
-},
+                        },
 
-    {
-        "data": [
                         {
                     "name": "The Empire Strikes Back",
                     "year": 1980,
                     "type": "Classic",
                     "sequential": 2,
                     "index": 1
-                        }
-                    ]
-},                 
+                        }, 
 
-    {
-        "data": [
                         {
                     "name": "Return of The Jedi",
                     "year": 1983,
                     "type": "Classic",
                     "sequential": 6,
                     "index": 2
-                        }
-                    ]        
-},
-
-    {
-        "data": [
-                        {
-                    "name": "The Phantom Menace",
-                    "year": 1999,
-                    "type": "Prequel",
-                    "sequential": 3,
-                    "index": 3
-                        }
-                    ]
-},
-
-    {
-        "data": [
+                        },
+        
                         {
                     "name": "Atack of The Clones",
                     "year": 2002,
                     "type": "Prequel",
                     "sequential": 4,
-                    "index": 4
-                        }
-                    ]
-},
+                    "index": 3
+                        },
 
-    {
-        "count": 6,
-        "data": [
                         {
                     "name": "Revenge of The Sith",
                     "year": 2005,
                     "type": "Prequel",
                     "sequential": 5,
                     "index": 5
-                        }
-                    ]
-},
+                        },
 
-    {
-        "data": [
                         {
                     "name": "The Force Awakens",
                     "year": 2015,
                     "type": "Sequel",
                     "sequential": 7,
                     "index": 6
-                        }
-                    ]
-},
+                        },
 
-    {
-        "data": [
                         {
                     "name": "The Last Jedi",
                     "year": 2017,
                     "type": "Sequel",
                     "sequential": 8,
                     "index": 7
-                        }
-                    ]
-},
+                        },
 
-    {
-        "data": [
                         {
                     "name": "The Rise of Skywalker",
                     "year": 2019,
                     "type": "Sequel",
                     "sequential": 9,
                     "index": 8
-                        }
-                    ]
-},
-    
-    {
+                        },
 
-        "data": [
                         {
                     "name": "Rogue One",
                     "year": 2016,
                     "type": "Spin-Off",
                     "sequential": 10,
                     "index": 9
-                        }
-                    ]
-},
-    
-    {
+                        },
 
-        "data": [
                         {
                     "name": "Han Solo",
                     "year": 2018,
@@ -133,9 +82,9 @@ const movieList = [
                     "index": 10
                         }
                     ]
-},
+}
 
-]
+
     
 
-module.exports =  movieList 
+module.exports =  movieList
